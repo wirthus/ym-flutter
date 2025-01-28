a4838147acbd45f22a70b5130c008a79c45d78b2
W <wirthus@mail.ru>
2025-01-28 22:40:51 +0300

    fps

--- android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.mobile
 
+import android.view.WindowManager
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        // Установка режима 90 Гц (если поддерживается)
+        window.attributes.preferredRefreshRate = 90f
+    }
+}
