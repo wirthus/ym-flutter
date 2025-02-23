b2635aa9fdcb82e8cdc32d7cf4e3c3920555ca06
W <wirthus@mail.ru>
2025-02-23 22:33:28 +0300

    Update NoInternetScreen routing and import statements

--- lib/core/presentation/pages/no_internet_screen/no_internet_screen.dart
+++ lib/core/presentation/pages/no_internet_screen/no_internet_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yagodmarket/core/presentation/widgets/responsive_widgets/responsive_layouts.dart';
+import 'package:yagodmarket/core/utils/riverpod_framework.dart';
 
-import '../../routing/app_router.dart';
-import '../../utils/riverpod_framework.dart';
-import '../../widgets/responsive_widgets/responsive_layouts.dart';
 import 'no_internet_screen_compact.dart';
 
 class NoInternetScreen extends HookConsumerWidget {
@@ -12,7 +11,7 @@ class NoInternetScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return PopScope(
       canPop: false,
-      onPopInvokedWithResult: (_, __) => const SplashRoute().go(context),
+      // onPopInvokedWithResult: (_, __) => const SplashRoute().go(context),
       child: WindowClassLayout(
         compact: (_) => OrientationLayout(
           portrait: (_) => const NoInternetScreenCompact(),
