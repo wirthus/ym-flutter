9a95840f2f5b579cfdd3a338b8640607388da634
W <wirthus@mail.ru>
2025-02-23 18:00:35 +0300

    Reorganize project assets and update asset paths

--- lib/features/register/presentation/register_page.dart
+++ lib/features/register/presentation/register_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yagodmarket/di.dart';
 import 'package:yagodmarket/features/register/presentation/bloc/register_wizard_cubit.dart';
 import 'package:yagodmarket/features/register/presentation/bloc/register_wizard_state.dart';
---
+++ lib/gen/my_assets.dart
@@ -0,0 +1,137 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class MyAssets {
+  const MyAssets._();
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\camera.png)
+  static const String ASSETS_ICONS_CAMERA_PNG = 'assets/icons/camera.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\delivery_icon.png)
+  static const String ASSETS_ICONS_DELIVERY_ICON_PNG =
+      'assets/icons/delivery_icon.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\ask.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_ASK_PNG =
+      'assets/icons/dialog_widget_icons/ask.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\confirmation.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_CONFIRMATION_PNG =
+      'assets/icons/dialog_widget_icons/confirmation.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\error.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_ERROR_PNG =
+      'assets/icons/dialog_widget_icons/error.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\info.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_INFO_PNG =
+      'assets/icons/dialog_widget_icons/info.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\info_reverse.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_INFO_REVERSE_PNG =
+      'assets/icons/dialog_widget_icons/info_reverse.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\location.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_LOCATION_PNG =
+      'assets/icons/dialog_widget_icons/location.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\no_header.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_NO_HEADER_PNG =
+      'assets/icons/dialog_widget_icons/no_header.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\success.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_SUCCESS_PNG =
+      'assets/icons/dialog_widget_icons/success.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\dialog_widget_icons\warrnig.png)
+  static const String ASSETS_ICONS_DIALOG_WIDGET_ICONS_WARRNIG_PNG =
+      'assets/icons/dialog_widget_icons/warrnig.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\languages_icons\arabic.png)
+  static const String ASSETS_ICONS_LANGUAGES_ICONS_ARABIC_PNG =
+      'assets/icons/languages_icons/arabic.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\languages_icons\english.png)
+  static const String ASSETS_ICONS_LANGUAGES_ICONS_ENGLISH_PNG =
+      'assets/icons/languages_icons/english.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\screens_icons\language.png)
+  static const String ASSETS_ICONS_SCREENS_ICONS_LANGUAGE_PNG =
+      'assets/icons/screens_icons/language.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\screens_icons\profile.png)
+  static const String ASSETS_ICONS_SCREENS_ICONS_PROFILE_PNG =
+      'assets/icons/screens_icons/profile.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\icons\screens_icons\settings.png)
+  static const String ASSETS_ICONS_SCREENS_ICONS_SETTINGS_PNG =
+      'assets/icons/screens_icons/settings.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\core\app_logo.png)
+  static const String ASSETS_IMAGES_CORE_APP_LOGO_PNG =
+      'assets/images/core/app_logo.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\core\custom_splash.png)
+  static const String ASSETS_IMAGES_CORE_CUSTOM_SPLASH_PNG =
+      'assets/images/core/custom_splash.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\core\custom_splash_animation.json)
+  static const String ASSETS_IMAGES_CORE_CUSTOM_SPLASH_ANIMATION_JSON =
+      'assets/images/core/custom_splash_animation.json';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\core\loading.gif)
+  static const String ASSETS_IMAGES_CORE_LOADING_GIF =
+      'assets/images/core/loading.gif';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\core\loading_animation.json)
+  static const String ASSETS_IMAGES_CORE_LOADING_ANIMATION_JSON =
+      'assets/images/core/loading_animation.json';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\core\no_internet.png)
+  static const String ASSETS_IMAGES_CORE_NO_INTERNET_PNG =
+      'assets/images/core/no_internet.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\login\hi_hand.png)
+  static const String ASSETS_IMAGES_LOGIN_HI_HAND_PNG =
+      'assets/images/login/hi_hand.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\login\login_background.png)
+  static const String ASSETS_IMAGES_LOGIN_LOGIN_BACKGROUND_PNG =
+      'assets/images/login/login_background.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\arrow.png)
+  static const String ASSETS_IMAGES_OTHERS_ARROW_PNG =
+      'assets/images/others/arrow.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\cluster.png)
+  static const String ASSETS_IMAGES_OTHERS_CLUSTER_PNG =
+      'assets/images/others/cluster.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\place.png)
+  static const String ASSETS_IMAGES_OTHERS_PLACE_PNG =
+      'assets/images/others/place.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\route_end.png)
+  static const String ASSETS_IMAGES_OTHERS_ROUTE_END_PNG =
+      'assets/images/others/route_end.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\route_start.png)
+  static const String ASSETS_IMAGES_OTHERS_ROUTE_START_PNG =
+      'assets/images/others/route_start.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\route_stop_by.png)
+  static const String ASSETS_IMAGES_OTHERS_ROUTE_STOP_BY_PNG =
+      'assets/images/others/route_stop_by.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\images\others\user.png)
+  static const String ASSETS_IMAGES_OTHERS_USER_PNG =
+      'assets/images/others/user.png';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\map_styles\dark_map.json)
+  static const String ASSETS_MAP_STYLES_DARK_MAP_JSON =
+      'assets/map_styles/dark_map.json';
+
+  /// ![preview](file://C:\_Projects\_wirthus\ym_flutter\assets\map_styles\light_map.json)
+  static const String ASSETS_MAP_STYLES_LIGHT_MAP_JSON =
+      'assets/map_styles/light_map.json';
+}
